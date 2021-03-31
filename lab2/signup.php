862f940b7cf0a5182af406323253106c6dd60da5
Your Name <you@example.com>
2021-03-31 19:31:06 +0300

    lab 3 Ajax form data.

--- lab2/signup.php
+++ lab2/signup.php
@@ -21,37 +21,6 @@ require_once("includes/processreg.php");
     
 </head>
 
-<script type="text/javascript">
-    $(document).ready(function() {
-        //use button click event
-        $("#btn").click(function() {
-            //get all inpuf field values
-            var name = $("#uname").val();
-            var email = $("#email").val();
-            var password = $("#pwd").val();
-            var city = $("#city").val();
-            var profile = $("#pp").val();
-            //call ajax here
-            $.post("includes/processreg.php", {
-                    name: name,
-                    email: email,
-                    password: password,
-                    city: city,
-                    profile: profile
-                },
-
-                function(data, status) {
-                    //check result
-                    if (data === "success") {
-                        $("#response").html("<div class='alert alert-info'>" + data + "</div>");
-                    } else {
-                        $("#response").html("<div class='alert alert-warning'>" + data + "</div>");
-                    }
-
-                });
-        });
-    });
-</script>
 
 <body>
 

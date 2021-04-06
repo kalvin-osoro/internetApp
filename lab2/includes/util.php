0766f27e41e1117a081c3cedf63a0d6d70862d7e
kalvin-osoro <62253122+kalvin-osoro@users.noreply.github.com>
2021-04-06 15:49:15 +0300

    Delete lab2 directory

--- lab2/includes/util.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Util {
-        //About DB
-        static $DB_NAME = "iap";
-        static $DB_USER = "root";
-        static $DB_USER_PASS = "";
-        static $SERVER_NAME = "localhost";
-    }
-?>
